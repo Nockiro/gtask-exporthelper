9d497cb2131a3c464ba71ea2b9dd1efc9e98d0d8
Nockiro <info@nockiro.de>
2022-04-12 14:57:54 +0200

    File/Namespace renaming

--- GTI.Core.Contracts/IGoogleTaskWriter.cs
+++ GTI.Core.Contracts/IGoogleTaskWriter.cs
@@ -5,6 +5,10 @@ namespace GTI.Core.Services
 {
     public interface IGoogleTaskWriter
     {
+        /// <summary>
+        /// Writes the given list of <see cref="GoogleTaskList"/> to a target defined by the class implementing this interface.
+        /// </summary>
+        /// <param name="listsToWrite">List with Google Task lists to write.</param>
         void Write(List<GoogleTaskList> listsToWrite);
     }
 }
